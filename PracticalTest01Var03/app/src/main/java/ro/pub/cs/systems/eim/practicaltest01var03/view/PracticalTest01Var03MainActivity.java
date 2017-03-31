d8b82568eb8f33bc67fe26513c3dc6b3bf42633b
Student User <student@cti.pub.ro>
2017-03-31 14:44:19 +0300

    Pana la A2 inclusiv

--- PracticalTest01Var03/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/view/PracticalTest01Var03MainActivity.java
+++ PracticalTest01Var03/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/view/PracticalTest01Var03MainActivity.java
@@ -1,9 +1,17 @@
-package ro.pub.cs.systems.eim.practicaltest01var03;
+package ro.pub.cs.systems.eim.practicaltest01var03.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import ro.pub.cs.systems.eim.practicaltest01var03.R;
 
 public class PracticalTest01Var03MainActivity extends AppCompatActivity {
+
+    private EditText rightEditText = null;
+    private Button leftButton = null;
+    private Button navigateToSecondaryActivityButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ PracticalTest01Var03/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var03/view/PracticalTest01Var03SecondaryActivity.java
@@ -0,0 +1,8 @@
+package ro.pub.cs.systems.eim.practicaltest01var03.view;
+
+/**
+ * Created by student on 31.03.2017.
+ */
+
+public class PracticalTest01Var03SecondaryActivity {
+}
